5a819304ec5911b91e4b6663d13cc32814044315
matijagojkovic <gojkovicmatija99@gmail.com>
2022-03-27 17:01:16 +0200

    sonar

--- src/main/java/rs/raf/demo/controllers/KnjizenjeController.java
+++ src/main/java/rs/raf/demo/controllers/KnjizenjeController.java
@@ -15,7 +15,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import rs.raf.demo.services.IService;
 
 import rs.raf.demo.services.impl.DnevnikKnjizenjaService;
 import rs.raf.demo.compare.CompareDnevnikKnjizenja;
