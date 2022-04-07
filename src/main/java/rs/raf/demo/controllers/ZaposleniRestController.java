a9783387c33621fe18a422caeb2b7c25d1045132
Marec99 <radisicm99@gmail.com>
2022-04-08 00:12:42 +0200

    Uradjen Crud zaposleni.

--- src/main/java/rs/raf/demo/controllers/ZaposleniRestController.java
+++ src/main/java/rs/raf/demo/controllers/ZaposleniRestController.java
@@ -1,13 +1,97 @@
 package rs.raf.demo.controllers;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.raf.demo.model.Staz;
+import rs.raf.demo.model.Zaposleni;
+import rs.raf.demo.model.enums.StatusZaposlenog;
+import rs.raf.demo.services.IStazService;
+import rs.raf.demo.services.IZaposleniService;
+import rs.raf.demo.specifications.RacunSpecificationsBuilder;
+
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @CrossOrigin
 @RestController
 @SecurityRequirement(name = "bearerAuth")
 @RequestMapping("/api/zaposleni")
 public class ZaposleniRestController {
+
+    private final IZaposleniService iZaposleniService;
+    private final IStazService iStazService;
+
+    public ZaposleniRestController(IZaposleniService iZaposleniService, IStazService iStazService) {
+        this.iZaposleniService = iZaposleniService;
+        this.iStazService = iStazService;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createZaposleni(@Valid @RequestBody Zaposleni zaposleni) {
+        return ResponseEntity.ok(iZaposleniService.save(zaposleni));
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateZaposleni(@Valid @RequestBody Zaposleni zaposleni,  @PathVariable Long id) {
+        if (iZaposleniService.findById(id).isPresent())
+            return ResponseEntity.ok(iZaposleniService.save(zaposleni));
+        throw new EntityNotFoundException();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> deleteZaposleni(@PathVariable("id") Long id) {
+        Optional<Zaposleni> optionalZaposleni = iZaposleniService.findById(id);
+        Zaposleni newZaposleni = new Zaposleni();
+        if (optionalZaposleni.isPresent()) {
+            optionalZaposleni.get().setStatusZaposlenog(StatusZaposlenog.NEZAPOSLEN);
+            for(Staz staz : optionalZaposleni.get().getStaz()){
+                if(staz.getKrajRada() == null){
+                    staz.setKrajRada(new Date());
+                    iStazService.save(staz);
+                }
+            }
+            return ResponseEntity.ok(iZaposleniService.save(optionalZaposleni.get()));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getZaposleniId(@PathVariable("id") Long id) {
+        Optional<Zaposleni> optionalZaposleni = iZaposleniService.findById(id);
+        if (optionalZaposleni.isPresent()) {
+            return ResponseEntity.ok(iZaposleniService.findById(id));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> search(@RequestParam(name = "search") String search) {
+        RacunSpecificationsBuilder<Zaposleni> builder = new RacunSpecificationsBuilder<>();
+
+        Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
+        Matcher matcher = pattern.matcher(search + ",");
+        while (matcher.find()) {
+            builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+        }
+
+        Specification<Zaposleni> spec = builder.build();
+
+        List<Zaposleni> result = iZaposleniService.findAll(spec);
+
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(result);
+    }
 }
--- src/main/java/rs/raf/demo/services/impl/ZaposleniService.java
+++ src/main/java/rs/raf/demo/services/impl/ZaposleniService.java
@@ -1,7 +1,59 @@
 package rs.raf.demo.services.impl;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import rs.raf.demo.model.Staz;
+import rs.raf.demo.model.Zaposleni;
+import rs.raf.demo.repositories.StazRepository;
+import rs.raf.demo.repositories.ZaposleniRepository;
+import rs.raf.demo.services.IZaposleniService;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
-public class ZaposleniService {
+public class ZaposleniService implements IZaposleniService{
+
+
+        private final ZaposleniRepository zaposleniRepository;
+        private StazService stazService;
+
+
+        public ZaposleniService(ZaposleniRepository zaposleniRepository) {
+            this.zaposleniRepository = zaposleniRepository;
+
+        }
+
+        @Override
+        public <S extends Zaposleni> S save(S zaposleni) {
+
+            Staz newStaz = new Staz();
+            newStaz.setPocetakRada(new Date());
+            stazService.save(newStaz);
+
+            zaposleni.getStaz().add(newStaz);
+
+            return zaposleniRepository.save(zaposleni);
+        }
+
+        @Override
+        public Optional<Zaposleni> findById(Long id) {
+            return zaposleniRepository.findById(id);
+        }
+
+        @Override
+        public List<Zaposleni> findAll() {
+            return zaposleniRepository.findAll();
+        }
+
+        @Override
+        public void deleteById(Long id) {
+            zaposleniRepository.deleteById(id);
+        }
+
+        @Override
+        public List<Zaposleni> findAll(Specification<Zaposleni> spec) {
+            return zaposleniRepository.findAll(spec);
+        }
 }
