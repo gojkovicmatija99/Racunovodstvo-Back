ec1eeebb23f0425e7119bcfc8d6ff58d809876c7
m-gojkovic <m.gojkovic@netconomy.net>
2022-06-30 21:40:30 +0200

    Exclude integration sonar

--- knjizenje/src/test/java/raf/si/racunovodstvo/knjizenje/controllers/SifraTransakcijeControllerIntegrationTest.java
+++ knjizenje/src/test/java/raf/si/racunovodstvo/knjizenje/controllers/SifraTransakcijeControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisabledIfEnvironmentVariable(named = "WORKSPACE", matches = "CI")
 class SifraTransakcijeControllerIntegrationTest {
 
     private final static String URI = "/api/sifraTransakcije";
--- user/src/test/java/raf/si/racunovodstvo/user/integration/PermissionIntegrationTest.java
+++ user/src/test/java/raf/si/racunovodstvo/user/integration/PermissionIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -40,6 +41,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DisabledIfEnvironmentVariable(named = "WORKSPACE", matches = "CI")
 class PermissionIntegrationTest {
 
     private final static String URI = "/api/permissions";
